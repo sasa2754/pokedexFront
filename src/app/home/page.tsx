0b9b9d52550d6391aae79bd532ffca0587b85984
sasa2754 <sasamor5@gmail.com>
2025-03-13 17:15:39 -0300

    ta pronto o mano gameboy

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import { Gameboy } from "@/components/gameboy";
-import nintendo from "../../../public/nintendo.png"
+import pokemon from "../../../public/pokemonFireRed.webp"
 import Image from "next/image";
 
 
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-neutral-500">
             <Gameboy>
-                a
+                <Image src={pokemon} alt="aaaa" className="flex justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
             </Gameboy>
 
         </div>
