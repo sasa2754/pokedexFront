d4545f89df27207606e164adf7b7db198d537c32
sasa2754 <sasamor5@gmail.com>
2025-05-23 16:42:17 -0300

    arrumei um errinho do front na parte de loading

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -202,70 +202,73 @@ const Register = () => {
             <div className="flex items-center justify-center min-h-screen bg-neutral-600">
                 <Gameboy buttonA={buttonA} buttonB={() => {if (page == 1) return; setPage(page - 1)}} buttonCima={buttonCima} buttonBaixo={buttonBaixo} buttonDireita={buttonDireita} buttonEsquerda={buttonEsquerda}>
                     <div className="flex relative justify-center bg-amber-300 w-full h-full rounded-xl">
-                        {loading && (
+                        {loading ? (
                             <div className="relative w-full flex items-center justify-center">
                                 <Image className="object-cover rounded-xl" src={cenarioFrenteLab} alt="aaa" fill></Image>
                                 <CircularProgress color="warning" size={80} className="absolute self-center rounded-xl"/>
                             </div>
-                        )}
-                        { page == 1 ? (
-                            <>
-                                <Image src={prof} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Bem vindo, novo treinador! É ótimo ter você aqui, vou te explicar um pouco sobre como o nosso mundo funciona..."></Baloon>
-                            </>
-
-                        ) : page == 2 ? (
-                            <>
-                                <Image src={profPokemon} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Nesse mundo nós temos criaturas chamadas de Pokemons, se tiver sorte, você pode capturá-los e cuidar deles..."></Baloon>
-                            </>
-                        ) : page == 3 ?(
-                            <>
-                                <Image src={profExplicando} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Mas o meu principal objetivo aqui é estudar os Pokemons, gostaria que você capturasse eles para que eu possa estudá-los melhor..."></Baloon>
-                            </>
-                        ) : page == 4 ? (
-                            <>
-                                <Image src={pokeballs} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Nós temos alguns tipos de pokeballs para você utilizar, cada uma tem uma taxa de eficiência diferente..."></Baloon>
-                            </>
-                        ) : page == 5 ?(
-                            <>
-                                <Image src={prof} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Mas para iniciarmos, preciso de algumas informações suas, mas não se preocupe, deixarei elas bem guardadas!"></Baloon>
-                            </>
-                        ) : page == 6 ? (
-                            <>
-                                <Image src={cenario1} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <div className="absolute h-full w-full flex text-black">
-                                    <div className="w-full flex flex-col items-center gap-3 self-end bg-amber-100 outline m-2 p-3 rounded">
-                                        <h1 className="text-xl font-bold text-red-600">Cadastro</h1>
-                                        <TextField onChange={ (e) => setName(e.target.value) } sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}} id="outlined-multiline-flexible" label="Nome"/>
-                                        <DatePicker onChange={ (value) => setBirthday(value) } label="Data de Nascimento" sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}}/>
-                                        <TextField onChange={ (e) => setEmail(e.target.value) } sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}} id="outlined-multiline-flexible" label="Email"/>
-                                        <TextField onChange={ (e) => setPass(e.target.value) } sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}} id="outlined-multiline-flexible" label="Senha" type="password"/>
-                                        <Link className="self-end text-red-600 hover:text-red-800 active:text-red-900" href={ROUTES.login}>Já em uma conta?</Link>
-                                    </div>
-                                </div>
-                            </>
-                        ) : page == 7 ? (
-                            <>
-                                <Image src={cenario1} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <div className="scrollhost absolute grid items-center justify-center grid-cols-4 gap-2 md:p-3 p-1 w-full h-full max-h-full overflow-auto rounded-lg">
-                                    {avatares?.map((avatar, index) => (
-                                      <Avatar index={index} key={index} name={avatar.name} url={avatar.url} activeIndex={coord.getIndex()}></Avatar>  
-                                    ))}
-                                </div>
-                            </>
-                        ) : page == 8 ? (
-                            <>
-                                <Image src={profTriste} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Parece que aconteceu um problema aqui, não consegui criar a sua conta, tente novamente mais tarde!"></Baloon>
-                            </>
                         ) : (
                             <>
-                                <Image src={profTriste} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
-                                <Baloon classExtra="absolute" text="Parece que estou com problemas no sistema do cadastro, vou arrumar o mais rápido possível, volte mais tarde!"></Baloon>
+                                { page == 1 ? (
+                                    <>
+                                        <Image src={prof} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Bem vindo, novo treinador! É ótimo ter você aqui, vou te explicar um pouco sobre como o nosso mundo funciona..."></Baloon>
+                                    </>
+        
+                                ) : page == 2 ? (
+                                    <>
+                                        <Image src={profPokemon} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Nesse mundo nós temos criaturas chamadas de Pokemons, se tiver sorte, você pode capturá-los e cuidar deles..."></Baloon>
+                                    </>
+                                ) : page == 3 ?(
+                                    <>
+                                        <Image src={profExplicando} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Mas o meu principal objetivo aqui é estudar os Pokemons, gostaria que você capturasse eles para que eu possa estudá-los melhor..."></Baloon>
+                                    </>
+                                ) : page == 4 ? (
+                                    <>
+                                        <Image src={pokeballs} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Nós temos alguns tipos de pokeballs para você utilizar, cada uma tem uma taxa de eficiência diferente..."></Baloon>
+                                    </>
+                                ) : page == 5 ?(
+                                    <>
+                                        <Image src={prof} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Mas para iniciarmos, preciso de algumas informações suas, mas não se preocupe, deixarei elas bem guardadas!"></Baloon>
+                                    </>
+                                ) : page == 6 ? (
+                                    <>
+                                        <Image src={cenario1} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <div className="absolute h-full w-full flex text-black">
+                                            <div className="w-full flex flex-col items-center gap-3 self-end bg-amber-100 outline m-2 p-3 rounded">
+                                                <h1 className="text-xl font-bold text-red-600">Cadastro</h1>
+                                                <TextField onChange={ (e) => setName(e.target.value) } sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}} id="outlined-multiline-flexible" label="Nome"/>
+                                                <DatePicker onChange={ (value) => setBirthday(value) } label="Data de Nascimento" sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}}/>
+                                                <TextField onChange={ (e) => setEmail(e.target.value) } sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}} id="outlined-multiline-flexible" label="Email"/>
+                                                <TextField onChange={ (e) => setPass(e.target.value) } sx={{ width: "100%", height: "56px", "& .MuiOutlinedInput-root": { "& fieldset": { borderColor: "red", borderStyle: "dashed", borderWidth: "2px" }, "&:hover fieldset": { borderColor: "red" }, "&.Mui-focused fieldset": { borderColor: "red" }, "& .MuiInputLabel-root.Mui-focused": { color: "orange" }, "& .MuiInputLabel-root": { color: "purple"} },}} id="outlined-multiline-flexible" label="Senha" type="password"/>
+                                                <Link className="self-end text-red-600 hover:text-red-800 active:text-red-900" href={ROUTES.login}>Já em uma conta?</Link>
+                                            </div>
+                                        </div>
+                                    </>
+                                ) : page == 7 ? (
+                                    <>
+                                        <Image src={cenario1} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <div className="scrollhost absolute grid items-center justify-center grid-cols-4 gap-2 md:p-3 p-1 w-full h-full max-h-full overflow-auto rounded-lg">
+                                            {avatares?.map((avatar, index) => (
+                                            <Avatar index={index} key={index} name={avatar.name} url={avatar.url} activeIndex={coord.getIndex()}></Avatar>  
+                                            ))}
+                                        </div>
+                                    </>
+                                ) : page == 8 ? (
+                                    <>
+                                        <Image src={profTriste} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Parece que aconteceu um problema aqui, não consegui criar a sua conta, tente novamente mais tarde!"></Baloon>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Image src={profTriste} alt="aaaa" className="flex object-cover grayscale-50 inset-shadow-2xs justify-center bg-gray-400 h-72 md:min-h-96 rounded-xl inset-shadow-2xl inset-shadow-gray-800"></Image>
+                                        <Baloon classExtra="absolute" text="Parece que estou com problemas no sistema do cadastro, vou arrumar o mais rápido possível, volte mais tarde!"></Baloon>
+                                    </>
+                                )}
                             </>
                         )}
                     </div>
